f98a542607496848df8a12a08a20e6b2955abd26
bittt <colemeyersiscool@gmail.com>
2025-10-03 19:58:38 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/bare/',                             // must match backend prefix
-    bare: 'https://ult-66t6.onrender.com',        // your Render backend URL
+    prefix: '/bare/',
+    bare: 'https://ult-66t6.onrender.com',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv.handler.js',
